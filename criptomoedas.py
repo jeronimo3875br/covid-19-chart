3bb23333504f65c7eade2db62d94c3fc30f85a13
Gabriel Jerônimo <63810093+jeronimo3875br@users.noreply.github.com>
2020-06-22 00:46:17 -0300

    Update

---
+++ criptomoedas.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math as mt
+import requests as req
+import json
+
+class Criptomoedas():
+	
+	def __init__(self, url):
+		self.url = url
+		pass
+	
+	def getValues(self):
+		
+		data = {
+			"BTC": req.get(self.url.format("BTC")).json(),
+			"ETH": req.get(self.url.format("ETH")).json(),
+			"LTC":  req.get(self.url.format("LTC")).json(),
+			"XRP": req.get(self.url.format("XRP")).json()
+		}
+		
+		self.createChart(data)
+		pass
+		
+	def createChart(self, data):
+		
+		self.data = data;
+		
+		plt.barh(
+			["Bitcoin", "Ethereun", "Litecoin", "Ripple"],
+			[
+				self.data["BTC"]["ticker"]["last"],
+				self.data["ETH"]["ticker"]["last"],
+				self.data["LTC"]["ticker"]["last"],
+				self.data["XRP"]["ticker"]["last"],
+			],
+			0.4,
+			align="center",
+			alpha=0.7,
+			label="growth by price",
+			color="purple"
+		)
+		plt.style.use("ggplot")
+		plt.title("Criptomoedas")
+		plt.ylabel("Price")
+		plt.xlabel("Criptomoeda")
+		plt.legend()
+		plt.show()
+		
+		pass
+
+generateChart = Criptomoedas("https://www.mercadobitcoin.net/api/{}/ticker")
+generateChart.getValues()
